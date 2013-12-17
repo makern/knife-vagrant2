a15aa977745275f8cbd58d4bcdd9dcba6317f542
Jazeps Basko <jazeps.basko@techtopia.com>
2013-12-17 11:52:58 +0000

    Adds port forwarding option like in the original knife-vagrant

--- lib/chef/knife/vagrant_server_create.rb
+++ lib/chef/knife/vagrant_server_create.rb
@@ -138,11 +138,22 @@ class Chef
         :description => "Use this IP address for the new instance"
 #        :proc => Proc.new { |ip| Chef::Config[:knife][:ip_address] = ip }
 
+      option :port_forward,
+        :short => '-p PORTS',
+        :long => '--port-forward PORTS',
+        :description => "Port forwarding.  Host port, VM port separated by a colon.  E.G. to forward 9000 on the host machine to 80 on the VM, -p 9000:80.  To list multiple forwards separate with a comma",
+        :proc => lambda { |o| Hash[o.split(/,/).collect { |a| a.split(/:/) }] },
+        :default => {}
+
       option :host_key_verify,
         :long => "--[no-]host-key-verify",
         :description => "Verify host key, enabled by default.",
         :boolean => true,
         :default => true
+
+      def build_port_forwards(ports)
+        ports.collect { |k, v| "config.vm.network :forwarded_port, host: #{k}, guest: #{v}" }.join("\n")
+      end
 
       def run
         $stdout.sync = true
@@ -200,6 +211,8 @@ Vagrant.configure("2") do |config|
   config.vm.hostname = "#{@server.name}"
 
   config.vm.network :private_network, ip: "#{@server.ip_address}"
+
+  #{build_port_forwards(config[:port_forward])}
 
   #{shares.join("\n")}
   
